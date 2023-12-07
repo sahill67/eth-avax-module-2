06a27376c544bd947130b59384417ce266d903a0
sahill67 <150814702+sahill67@users.noreply.github.com>
2023-12-07 12:56:52 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,18 +1,15 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
-  const initBalance = 1;
+  // Set the desired initial balance and PIN
+  const initBalance = 1000;
+  const _pin = 1001;
+
   const Assessment = await hre.ethers.getContractFactory("Assessment");
-  const assessment = await Assessment.deploy(initBalance);
+  const assessment = await Assessment.deploy(initBalance, _pin);
   await assessment.deployed();
 
-  console.log(`A contract with balance of ${initBalance} eth deployed to ${assessment.address}`);
+  console.log(`A contract with balance of ${initBalance} ETH and PIN ${_pin} deployed to ${assessment.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
